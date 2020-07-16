c3521e38740cda5dc5c8ab271075c1e68de6ee3a
M Groesbeck <m.groesbeck@gmail.com>
2020-07-16 10:18:37 -0700

    Started on max_heap; not yet working.

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,61 @@
 class Heap:
     def __init__(self):
         self.storage = []
+    
+    def get_parent(self, index):
+        if index == 0:
+            return None
+        else:
+            return ((index + (index % 2)) / 2) - 1
+    
+    def swap(self, x, y):
+        val = self.storage[x]
+        self.storage[x] = self.storage[y]
+        self.storage[y] = val
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        bubbler = self._bubble_up(len(self.storage)-1)
+        while bubbler:
+            bubbler = self._bubble_up(bubbler)
 
     def delete(self):
+        # index = 0
+        # while index*2 + 1 < len(self.storage):
+        #     if self.storage[index*2 + 1] > self.storage[index*2 + 2]:
+        #         child_ind = index*2 + 1
+        #     else:
+        #         child_ind = index*2 + 2
+        #     self.swap(index, child_ind)
+        #     index = child_ind
+        # if index*2 + 2 == len(self.storage):
+        #     self.swap(index, index*2 + 1)
+        # self.storage.pop()
         pass
-
+        
     def get_max(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        parent = self.get_parent(index)
+        if self.storage[index] > self.storage[parent]:
+            self.swap(index, parent)
+            return parent
+        else:
+            return False
 
     def _sift_down(self, index):
-        pass
+        if self.storage[index*2 + 1] > self.storage[index*2 + 2]:
+            child_ind = index*2 + 1
+            child_val = self.storage[index*2 + 1]
+        else:
+            child_ind = index*2 + 2
+            child_val = self.storage[index*2 + 2]
+        if child_val > self.storage[index]:
+            self.swap(index, child_ind)
+            return child_ind
+        else:
+            return False
